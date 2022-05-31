d1a944809baca4cede1da59dccef19abe30800f9
Cristian Cajiao <cristiancajiaos@outlook.com>
2022-05-31 16:47:32 -0400

    Cambio de sistema de HeroService a Observable

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { HEROES } from './constants/mock-heroes';
 import { Hero } from './interfaces/hero';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class HeroService {
 
   constructor() { }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
   }
 }
