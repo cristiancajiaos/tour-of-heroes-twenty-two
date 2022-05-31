50d13227fb73688da0ffc08a2c5a404f9e0375c9
Cristian Cajiao <cristiancajiaos@outlook.com>
2022-05-31 17:46:14 -0400

    Creado servicio messagesServices, unido al servicio heroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,15 +2,20 @@ import { HEROES } from './constants/mock-heroes';
 import { Hero } from './interfaces/hero';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { MessagesService } from './messages/messages.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(
+    private messagesService: MessagesService
+  ) { }
 
   getHeroes(): Observable<Hero[]> {
-    return of(HEROES);
+    const heroes = of(HEROES);
+    this.messagesService.add('HeroService: Fetched heroes')
+    return heroes;
   }
 }
