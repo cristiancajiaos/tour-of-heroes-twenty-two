02f03255e54f3149c4212160169c70e486d1d836
Cristian Cajiao <cristiancajiaos@outlook.com>
2022-06-01 16:46:15 -0400

    Adaptación de servicio HeroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,26 +3,34 @@ import { Hero } from './interfaces/hero';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { MessagesService } from './messages/messages.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
+  private heroesUrl = 'api/heroes';
+  
   constructor(
+    private http: HttpClient,
     private messagesService: MessagesService
   ) { }
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
-    this.messagesService.add('HeroService: Fetched heroes')
+    this.log(`Fetched heroes`);
     return heroes;
   }
 
   getHero(id: number): Observable<Hero> {
     // Por ahora, se asume un ´heroe con una ID especificada existe
     const hero = HEROES.find(h => h.id === id)!;
-    this.messagesService.add(`HeroService: fetched hero id=${id}`);
+    this.log(`fetched hero id=${id}`);
     return of(hero);
+  }
+
+  private log(message: string) {
+    this.messagesService.add(`HeroService: ${message}`);
   }
 }
